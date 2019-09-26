896d981f857fddc328e80d06a296b7d6549d6ee5
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2019-09-26 16:48:32 -0700

    Adding Update and Delete
    Adding Update method and template
    Adding Delete method
    Cleanning up dashboard

--- apps/wally_trips/views.py
+++ apps/wally_trips/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect, HttpResponseRedirect
 import requests, json
 from .models import Trips
-from ..login_and_reg_app.models import User
+from ..wally_trips.models import User
 from django.utils.dateparse import parse_date
 
 # Create your views here.
@@ -12,9 +12,9 @@ def wally_index(request):
         return render(request, "wally_trips/index.html")
 
 
-def trip_weather(request):
+def trip_weather(request, trip_id):
     url = "https://community-open-weather-map.p.rapidapi.com/weather"
-    trip = Trips.objects.get(id=request.POST['trip_id'])
+    trip = Trips.objects.get(id=trip_id)
     querystring = {"q": trip.destination}
     headers = {
         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
@@ -26,7 +26,21 @@ def trip_weather(request):
         print(i)
     weather_info = response_json["weather"]
     name = response_json["name"]
-    context = {"cityname": name, "w": weather_info[0]}
+
+    print("Dashboard - viewing trips:----")
+    if "user_id" in request.session:
+        all_user_trips = Trips.objects.filter(
+            user_id_id=request.session["user_id"]
+        ).order_by("-id")
+        all_other_trips = Trips.objects.exclude(user_id_id=request.session["user_id"])
+    else:
+        return redirect("/")
+    context = {
+        "all_user_trips": all_user_trips,
+        "all_other_trips": all_other_trips,
+        "cityname": name,
+        "w": weather_info[0],
+    }
     return render(request, "wally_trips/index.html", context)
 
 
@@ -81,13 +95,47 @@ def view_trip(request):
     pass
 
 
-def trip_edit(request):
+def trip_edit(request, trip_id):
+    trip = Trips.objects.get(id=trip_id)
+    print(trip.destination)
+    context = {
+        "trip_info": trip,
+        "start_date": trip.start_date.strftime("%Y-%m-%d"),
+        "end_date": trip.end_date.strftime("%Y-%m-%d")
+    }
+    return render(request, "wally_trips/Update.html", context)
+
+
+def trip_update(request, trip_id):
+    if request.method == "POST":
+        # Pass the post data to the mehod we wrote and save the response in a variable called errors
+        errors = Trips.objects.basic_validator(request.POST)
+
+        # check if the errors dictionary has anything in it
+        if len(errors) > 0:
+            # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+            for key, value in errors.items():
+                print(value)
+                messages.error(request, value)
+            return redirect("/trips/new")
+        else:
+            trip = Trips.objects.get(id=trip_id)
+
+            trip.destination = request.POST['destination']
+            trip.start_date = parse_date(request.POST['start_date'])
+            trip.end_date = parse_date(request.POST['end_date'])
+            trip.plan = request.POST['plan']
+            trip.save()
+
+            print("-----------------")
+            print(trip.destination)
+            # newtrip = "/trips/" + str(new_trip.id)
+            return redirect("/dashboard")
     pass
 
 
-def trip_update(request):
-    pass
-
-
-def trip_remove(request):
-    pass
+def trip_remove(request, trip_id):
+    rem = Trips.objects.get(id=trip_id)
+    print(f"deleting a record {str(trip_id)}")
+    rem.delete()
+    return redirect("/dashboard")
